c3c7d85f6fe6370d963625090cf7d62b5df40d87
Lemonef <sudha.sutaschuto@gmail.com>
2026-03-22 18:14:48 +0700

    Add remaining Django app files: __init__.py, admin.py, apps.py, migrations, tests.py

---
+++ app/admin.py
@@ -0,0 +1,54 @@
+from django.contrib import admin
+from django.utils.html import format_html
+from app.models import (
+    Creator,
+    Listener,
+    Library,
+    Form,
+    Song,
+    Share,
+    CreditTransaction,
+)
+from app.services.generation_service import create_mock_song_from_form
+
+
+@admin.register(Form)
+class FormAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "creator",
+        "genre",
+        "mood",
+        "requested_title",
+        "requested_duration_seconds",
+        "created_at",
+    )
+
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+        if not change:
+            create_mock_song_from_form(obj)
+
+
+@admin.register(Library)
+class LibraryAdmin(admin.ModelAdmin):
+    filter_horizontal = ("songs",)
+
+@admin.register(Share)
+class ShareAdmin(admin.ModelAdmin):
+    list_display = ("id", "song", "token", "clickable_share_link", "created_at")
+    readonly_fields = ("token", "clickable_share_link", "created_at")
+
+    def clickable_share_link(self, obj):
+        return format_html(
+            '<a href="{}" target="_blank">{}</a>',
+            obj.share_link,
+            obj.share_link
+        )
+
+    clickable_share_link.short_description = "Share Link"
+
+admin.site.register(Creator)
+admin.site.register(Listener)
+admin.site.register(Song)
+admin.site.register(CreditTransaction)
